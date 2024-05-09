6cb42b7208e79e1e4c3dea813dfe9f82988c7210
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-05-09 17:10:06 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
-
+  const [addedData, setAddedData] = useState({})
+  
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm  /> : <QuestionList  />}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList( ) {
+  
+  const [questions, setQuestions] = useState([])
+ 
+  function handleDelete(deletedId){
+    fetch(`http://localhost:4000/questions/${deletedId}`,{
+      method : 'DELETE'
+    })
+    .then((resp) => resp.json())
+    .then(() => {
+      const newQuestions = questions.filter((question) => {
+        if(question.id !== deletedId) {
+          return question
+        }
+      })
+      setQuestions(newQuestions)
+    })
+  }
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then((resp) => resp.json())
+    .then((data) => setQuestions(data))
+    
+  }, [])
+  console.log(questions)
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => {
+        return <QuestionItem key={question.id} question={question} handleDelete={handleDelete} />
+      })}</ul>
     </section>
   );
 }
